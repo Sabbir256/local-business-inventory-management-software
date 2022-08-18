312bdae7a46ca2ee0feb39f40435c3e16be8f3a2
sabbir <29686831+aboutsabbir@users.noreply.github.com>
2022-08-18 21:16:22 +0600

    login logout signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end
